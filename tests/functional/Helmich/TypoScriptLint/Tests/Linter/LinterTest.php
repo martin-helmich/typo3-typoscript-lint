f9e5f048b5d68bfe4c575d3fb009cd3bc772aa7b
Martin Helmich <m.helmich@mittwald.de>
2015-11-01 20:50:53 +0100

    Add functional test suite

---
+++ tests/functional/Helmich/TypoScriptLint/Tests/Linter/LinterTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Helmich\TypoScriptLint\Tests\Linter;
+
+
+class LinterTest extends \PHPUnit_Framework_TestCase
+{
+
+
+
+    public function testLinterCreatesExpectedOutput()
+    {
+        $this->assertEquals(TRUE, FALSE);
+    }
+
+
+
+}
