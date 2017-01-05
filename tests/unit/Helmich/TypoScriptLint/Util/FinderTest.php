7ecc228c9e69bd98b921b657727fd663072b9839
Martin Helmich <kontakt@martin-helmich.de>
2017-01-05 23:36:23 +0100

    Add test case for filename filter

--- tests/unit/Helmich/TypoScriptLint/Util/FinderTest.php
+++ tests/unit/Helmich/TypoScriptLint/Util/FinderTest.php
@@ -1,47 +1,51 @@
 <?php
 namespace Helmich\TypoScriptLint\Util;
 
+use org\bovigo\vfs\vfsStream;
+use Symfony\Component\Finder\Finder as SymfonyFinder;
+
 /**
- * @covers Helmich\TypoScriptLint\Util\Finder
+ * @covers \Helmich\TypoScriptLint\Util\Finder
  */
 class FinderTest extends \PHPUnit_Framework_TestCase
 {
+    public function setUp()
+    {
+        vfsStream::setup('root', null, [
+            'file1.typoscript' => '',
+            'file2.txt' => '',
+            'directory' => [
+                'file3.ts' => '',
+                'file4' => ''
+            ]
+        ]);
+    }
 
-    /**
-     * @requires PHP 5.6
-     */
     public function testFilenameListIsGenerated()
     {
-        $sfFinder   = $this->getMockBuilder('\Symfony\Component\Finder\Finder')->disableOriginalConstructor()->getMock(
-        );
-        $filesystem = $this->getMockBuilder('\Helmich\TypoScriptLint\Util\Filesystem')->disableOriginalConstructor(
-        )->getMock();
-
-        $fib = $this->getMockBuilder('SplFileInfo')->disableOriginalConstructor();
-        $fi1 = $fib->getMock();
-        $fi1->expects($this->any())->method('isFile')->willReturn(false);
-        $fi2 = $fib->getMock();
-        $fi2->expects($this->any())->method('isFile')->willReturn(true);
-        $fi3 = $fib->getMock();
-        $fi3->expects($this->any())->method('isFile')->willReturn(true);
-
-        $fi4 = $fib->getMock();
-        $fi4->expects($this->any())->method('getPathname')->willReturn('directory/file3');
-        $fi5 = $fib->getMock();
-        $fi5->expects($this->any())->method('getPathname')->willReturn('directory/file4');
-
-        $sfFinder->expects($this->once())->method('files')->willReturnSelf();
-        $sfFinder->expects($this->once())->method('in')->with('directory');
-        $sfFinder->expects($this->once())->method('getIterator')->willReturn(new \ArrayObject([$fi4, $fi5]));
+        $sfFinder = new SymfonyFinder();
+        $filesystem = new Filesystem();
 
         $finder = new Finder($sfFinder, $filesystem);
+        $filenames = $finder->getFilenames(['vfs://root/directory', 'vfs://root/file1.typoscript', 'vfs://root/file2.txt']);
 
-        $filesystem->expects($this->at(0))->method('openFile')->with('directory')->willReturn($fi1);
-        $filesystem->expects($this->at(1))->method('openFile')->with('file1')->willReturn($fi2);
-        $filesystem->expects($this->at(2))->method('openFile')->with('file2')->willReturn($fi3);
-
-        $filenames = $finder->getFilenames(['directory', 'file1', 'file2']);
-
-        $this->assertEquals(['directory/file3', 'directory/file4', 'file1', 'file2'], $filenames);
+        assertThat($filenames, equalTo([
+            'vfs://root/directory/file3.ts',
+            'vfs://root/directory/file4',
+            'vfs://root/file1.typoscript',
+            'vfs://root/file2.txt'
+        ]));
     }
+
+    public function testFilenamesAreFilteredByPatterns()
+    {
+        $sfFinder = new SymfonyFinder();
+        $filesystem = new Filesystem();
+
+        $finder = new Finder($sfFinder, $filesystem);
+        $filenames = $finder->getFilenames(['vfs://root'], ['*.ts']);
+
+        assertThat($filenames, self::equalTo(['vfs://root/directory/file3.ts']));
+    }
+
 }
