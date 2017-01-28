a5cfe508143f342698897cf08bbaadeaf568672b
Martin Helmich <kontakt@martin-helmich.de>
2017-01-28 18:55:55 +0100

    Clean up code formatting

--- src/Helmich/TypoScriptLint/Command/LintCommand.php
+++ src/Helmich/TypoScriptLint/Command/LintCommand.php
@@ -105,12 +105,7 @@ class LintCommand extends Command
             ->addOption('config', 'c', InputOption::VALUE_REQUIRED, 'Configuration file to use', 'tslint.yml')
             ->addOption('format', 'f', InputOption::VALUE_REQUIRED, 'Output format', 'text')
             ->addOption('output', 'o', InputOption::VALUE_REQUIRED, 'Output file ("-" for stdout)', '-')
-            ->addOption(
-                'exit-code',
-                'e',
-                InputOption::VALUE_NONE,
-                'Set this flag to exit with a non-zero exit code when there are warnings'
-            )
+            ->addOption('exit-code', 'e', InputOption::VALUE_NONE, 'Set this flag to exit with a non-zero exit code when there are warnings')
             ->addArgument('paths', InputArgument::OPTIONAL | InputArgument::IS_ARRAY, 'File or directory names. If omitted, the "paths" option from the configuration file will be used, if present');
     }
 
