ccf2bf53259702b8b216c90081a890f9117b9387
Tymoteusz Motylewski <t.motylewski@gmail.com>
2020-12-11 15:41:50 +0100

    Add sniff which check for config.no_cache=1 usages
    Co-authored-by: Martin Helmich <kontakt@martin-helmich.de>

---
+++ src/Linter/Sniff/Visitor/ConfigNoCacheVisitor.php
@@ -0,0 +1,58 @@
+<?php declare(strict_types=1);
+
+namespace Helmich\TypoScriptLint\Linter\Sniff\Visitor;
+
+use Helmich\TypoScriptLint\Linter\Report\Issue;
+use Helmich\TypoScriptLint\Linter\Sniff\ConfigNoCacheSniff;
+use Helmich\TypoScriptParser\Parser\AST\Operator\Assignment;
+use Helmich\TypoScriptParser\Parser\AST\Statement;
+
+class ConfigNoCacheVisitor implements SniffVisitor
+{
+    /** @var Issue[] */
+    private $issues = [];
+
+    /**
+     * @return Issue[]
+     */
+    public function getIssues(): array
+    {
+        return $this->issues;
+    }
+
+    public function enterTree(array $statements): void
+    {
+    }
+
+    public function enterNode(Statement $statement): void
+    {
+         if (!$statement instanceof Assignment) {
+             return;
+         }
+         if ($statement->object->relativeName !== 'no_cache'
+             && substr($statement->object->relativeName, -9) !== '.no_cache') {
+             return;
+         }
+         if ($statement->value->value !== '0') {
+             $this->issues[] = new Issue(
+                $statement->sourceLine,
+                null,
+                sprintf(
+                    'Setting config.no_cache = 1 is discouraged as it is bad for performance. '
+                        . 'Consider using USER_INT object instead. Found in path: %s',
+                    $statement->object->absoluteName
+                ),
+                Issue::SEVERITY_WARNING,
+                ConfigNoCacheSniff::class
+            );
+         }
+    }
+
+    public function exitNode(Statement $statement): void
+    {
+    }
+
+    public function exitTree(array $statements): void
+    {
+    }
+}
