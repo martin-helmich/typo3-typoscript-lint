309c12eac387e3a0e92218d4a06a14954652aaef
Martin Helmich <kontakt@martin-helmich.de>
2020-12-13 14:56:49 +0100

    Fix PHAR package testing workflow (#106)
    * Use PHP 7.4 for PHAR package test
    
    * Bump phar-composer to v1.2.0

--- .build/package.sh
+++ .build/package.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 VERSION="${1#v}"
+
+phar_composer_version="1.2.0"
 build_dir=".build/workspace"
 
 if [ -z "${VERSION}" ] ; then
@@ -16,14 +18,14 @@ mkdir -p "${build_dir}"
 cp -a typoscript-lint typoscript-lint.dist.yml services.yml src vendor composer.json composer.lock LICENSE "${build_dir}/" 
 cd "${build_dir}/vendor" && rm -rf */*/tests/ */*/src/tests/ */*/docs/ */*/*.md */*/composer.* */*/phpunit.* */*/.gitignore */*/.*.yml */*/*.xml && cd - >/dev/null
 
-if [ ! -f phar-composer-1.1.0.phar ] ; then
-  curl -JOL https://github.com/clue/phar-composer/releases/download/v1.1.0/phar-composer-1.1.0.phar
+if [ ! -f phar-composer-${phar_composer_version}.phar ] ; then
+  curl -JOL https://github.com/clue/phar-composer/releases/download/v${phar_composer_version}/phar-composer-${phar_composer_version}.phar
 fi
 
 phar_name="typoscript-lint-${VERSION}.phar"
 
-chmod +x phar-composer-*.phar
-./phar-composer-*.phar build "./${build_dir}" "${phar_name}"
+chmod +x phar-composer-${phar_composer_version}.phar
+./phar-composer-${phar_composer_version}.phar build "./${build_dir}" "${phar_name}"
 
 if [ -n "${signing_key}" ] ; then
   echo "${signing_key}" | gpg --import
